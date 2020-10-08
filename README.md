ee85b92efb591b3cbb7aab3c20d4ea79f0929622
musturi <rakesh.musturi.ext@nokia.com>
2020-10-08 17:49:07 +0530

    Updated readme file

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ This React repo is created using create-react-app.
 SCSS is used for styling.
 react-route-dom using for history.
 
+You can find the working heroku link below.
+[a link](https://spacex-rakesh.herokuapp.com/)
+
 All accessibility labels and structures along with aria-labels.
 You can see the courage using wave chrome extension
 ![Alt text](spacex.png?raw=true "Accessibility")
--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -7,25 +7,30 @@ class Details extends React.Component {
         const { items } = this.props;
         return (
         <div className={"DetailsDiv"}>
-            {items && items.map((data) => {
-                return (<div className={"productTile"}>
-                    <div className="imageBlock">
-                        <img style={{ width: '100%' }} alt={`SpaceX ${data.mission_name}`} src={data.links.mission_patch_small} />
-                    </div>
-                    <h5 style={{ color: "blue" }}>{data.mission_name}</h5>
-                    <div>
-                        <h5>Mission IDs: </h5>
-                        <ul style={{ color: "blue" }}>
-                            {data && data.mission_id && data.mission_id.map((k)=>{
-                                return (<li>{k}</li>)
-                            })}
-                        </ul>
-                    </div>
-                        <h5>{`Launch year: `}<span style={{ color: "blue" }}>{data.launch_year}</span></h5>
-                        <h5>{'Sucessful Launch: '}<span style={{ color: "blue" }}>{data.launch_success.toString()}</span></h5>
-                        <h5>{`Sucessful Landing: `}<span style={{ color: "blue" }}>{(data.rocket.first_stage.cores[0].land_success && data.rocket.first_stage.cores[0].land_success.toString()) || "false"}</span></h5>
-                </div>)
-            })}
+            {items.length > 0 ? <div>
+                {items && items.map((data) => {
+                    return (<div className={"productTile"}>
+                        <div className="imageBlock">
+                            <img style={{ width: '100%' }} alt={`SpaceX ${data.mission_name}`} src={data.links.mission_patch_small} />
+                        </div>
+                        <h5 style={{ color: "blue" }}>{data.mission_name}</h5>
+                        <div>
+                            <h5>Mission IDs: </h5>
+                            <ul style={{ color: "blue" }}>
+                                {data && data.mission_id && data.mission_id.map((k)=>{
+                                    return (<li>{k}</li>)
+                                })}
+                            </ul>
+                        </div>
+                            <h5>{`Launch year: `}<span style={{ color: "blue" }}>{data.launch_year}</span></h5>
+                            <h5>{'Sucessful Launch: '}<span style={{ color: "blue" }}>{data.launch_success.toString()}</span></h5>
+                            <h5>{`Sucessful Landing: `}<span style={{ color: "blue" }}>{(data.rocket.first_stage.cores[0].land_success && data.rocket.first_stage.cores[0].land_success.toString()) || "false"}</span></h5>
+                    </div>)
+                })}
+            </div> :
+            <div style={{ textAlign: "center", fontSize: "24px", padding: "10%" }}>
+                <h5>No Items(S) to display.</h5>
+            </div>}
         </div>
         )
     }
